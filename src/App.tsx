0683942111124078251f7f399bb33113f647abaa
Pedro Cantanhêde <pedroh.cantanhede@hotmail.com>
2026-03-26 22:35:30 -0300

    fix: routing adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { GameOver } from './pages/GameOver';
 function App() {
   return (
     <GameProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/game" element={<Game />} />
